5f3249e4d75fe8ed6c73f11f514a087832fa49f9
WilliamNerem <william-kn@hotmail.com>
2021-04-20 13:44:01 +0200

    Lagd database v1

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-Test
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class ProductModel(db.Model):
+    product_id = db.Column(db.Integer, primary_key=True)
+    productName = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    productInfoShort = db.Column(db.String(100), nullable=False)
+    productInfoLong = db.Column(db.String(1000), nullable=False)
+    productImage = db.Column(db.String(100), nullable=False)
