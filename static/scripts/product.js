735c0066c598e3e9af66a505003ce3d38e478329
Maren <maren.lund@wemail.no>
2021-04-23 10:40:34 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ static/scripts/product.js
@@ -0,0 +1,13 @@
+fetch('http://127.0.0.1:5000/product/productid')
+.then((res) => res.json())
+.then((data) => {
+    let output = `<h1> ${data.productName} </h1>`
+    output += `
+        <div>
+            <img src="{{ url_for('static', filename='${data.productImage}') }}" />
+            <p>Price: ${data.price}</p>
+            <p>Info: ${data.productInfoLong}</p>
+        </div>
+    `
+    document.getElementById('output').innerHTML = output;
+})
