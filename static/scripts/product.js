4a5d4c7128c048fc7142b66475407b09b6c8b4d3
Maren <maren.lund@wemail.no>
2021-05-18 18:04:02 +0200

    added carousel of product images on product info page

--- static/scripts/product.js
+++ static/scripts/product.js
@@ -10,79 +10,67 @@ if (product_exists == 'false') {
     </div>
     `
     document.getElementById('renderContent').innerHTML = output;
-    
+
 } else {
 
     fetch("http://localhost:5000/fetchCurrent")
-    .then((res) => res.json())
-    .then((data) => {
-        output = `
+        .then((res) => res.json())
+        .then((data) => {
+            output = `
             <h1 class="display-4"> ${data.productName}</h1>
             <p class="lead">${data.productInfoLong}</p> `
-            // <img src="../${data.productImage}" class="img-thumbnail" />
-            // fetch("http://localhost:5000/fetchCurrentImage")
-            // .then((res) => res.json())
-            // .then((data) => {
-            //     data.forEach(function(image) {
-            //     output += `<img src="../${image.productImage}" class="img-thumbnail" />`
-            //     })
-            // })
-        // output += `<div class="display-flex">
-        //     <p class="lead">${data.price},-</p>
-        //     <button class="addToCart btn btn-outline-dark" value="${data.product_id}">Add to cart</button>
-        // </div>
-        // `
-        // document.getElementById('renderContent').innerHTML = output;
-
-        // document.querySelectorAll('.addToCart').forEach(item => {
-        //     item.addEventListener('click', addToCart)
-        // })
-        // function addToCart() {
-        //     event.preventDefault();
-        //     let prodId = this.value
-        //     fetch("http://localhost:5000/shoppingcart/" + prodId)
-        //     .then(() => {     
-        //         checkItemsInCart();
-        //     })
-        // }
-        return fetch("http://localhost:5000/fetchCurrentImage")
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        data.forEach(function(image) {
-        output += `<img src="../${image.productImage}" class="img-thumbnail" />`
+            return fetch("http://localhost:5000/fetchCurrentImage")
         })
-        return fetch("http://localhost:5000/fetchCurrent")
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        output += `<div class="display-flex">
+        .then((res) => res.json())
+        .then((data) => {
+            output += `<div id="productsCarousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-inner">`
+            let firstIteration = true;
+            data.forEach(function (image) {
+                if (firstIteration) {
+                    output += `<div class="carousel-item active">
+                <img src="../${image.productImage}" class="d-block w-75 carousel-img">
+                </div>`
+                    firstIteration = false;
+                }
+                else {
+                    output += `<div class="carousel-item">
+                <img src="../${image.productImage}" class="d-block w-75 carousel-img">
+                </div>`
+                }
+
+            })
+            output += `  </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#productsCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#productsCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>`
+            return fetch("http://localhost:5000/fetchCurrent")
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            output += `<div class="display-flex">
             <p class="lead">${data.price},-</p>
             <button class="addToCart btn btn-outline-dark" value="${data.product_id}">Add to cart</button>
             </div>
             `
-        document.getElementById('renderContent').innerHTML = output;
+            document.getElementById('renderContent').innerHTML = output;
 
-        document.querySelectorAll('.addToCart').forEach(item => {
-            item.addEventListener('click', addToCart)
-        })
-        function addToCart() {
-            event.preventDefault();
-            let prodId = this.value
-            fetch("http://localhost:5000/shoppingcart/" + prodId)
-            .then(() => {     
-                checkItemsInCart();
+            document.querySelectorAll('.addToCart').forEach(item => {
+                item.addEventListener('click', addToCart)
             })
-        }
-    })
-    // fetch("http://localhost:5000/fetchCurrentImage")
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             data.forEach(function(image) {
-    //             output += `<img src="../${image.productImage}" class="img-thumbnail" />`
-    //             })
-    //             document.getElementById('renderContent').innerHTML = output;
-    //         })
-    
-    
+            function addToCart() {
+                event.preventDefault();
+                let prodId = this.value
+                fetch("http://localhost:5000/shoppingcart/" + prodId)
+                    .then(() => {
+                        checkItemsInCart();
+                    })
+            }
+        })
 }
